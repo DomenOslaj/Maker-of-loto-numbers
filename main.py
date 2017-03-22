74a520b56fab10602f737419c907359c1a0f3380
Domen Oslaj <doslaj@gmail.com>
2017-03-22 22:14:37 +0100

    Added try & except added warning when user enter the strings and numbers that are not number 7. Our loto is 7 numbers long :)

--- main.py
+++ main.py
@@ -16,25 +16,28 @@ def loto_length(length):            # makes random int numbers and put them to t
           break
 
         random_num = random.randint( 0, 50 )
-        if random_num not in loto_list:
+        if random_num not in loto_list:               #if not yet it loto_list, append it now.
             loto_list.append(random_num)
 
     return loto_list
 
-def main():                      #main function: prints, run a loop for number and then prints loto_lenght of ask_for_number
+def main():                      #main function: prints, run a loop for number and then prints loto_lenght of ask_int. Try and except.
     print "Welcome to the Lottery numbers generator."
 
     while True:
-        ask_for_number = int(raw_input("Please enter how many random numbers would you like to have: "))
-        if ask_for_number == 7:
-            break
+        ask_for_number = raw_input("Please enter how many random numbers would you like to have: ")
+        try:                                                        #try if you can transform string to a int number. That means that user entered a number and functions will work.
+            ask_int = int(ask_for_number)
+            if ask_int == 7:                                        #if user entered a number and this number is 7, continue with function logo_lenght + that number
+                print loto_length(ask_int)
+                print "END"
+                break
+            else:                                                   #if user did entered a number but this number is not 7, tell him that.
+                print "Loto cannot have %s numbers. Please choose another number. " % (ask_int)
 
-        else:
-            print "Loto cannot have %s numbers. Please choose another number. " % (ask_for_number)
+        except:                                                     #if user did NOT entered a number, you warn it.
+            print "You didnt entered a number."
 
-
-    print loto_length(ask_for_number)
-    print "END"
 
 if __name__ == "__main__":
     main()
